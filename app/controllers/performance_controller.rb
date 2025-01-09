baa7f3fa6f63f824db55770a154597c15ad6a2f7
Masato <a2422mt@aiit.ac.jp>
2025-01-09 21:41:35 +0900

    Create the function determining user attempts

--- app/controllers/performance_controller.rb
+++ app/controllers/performance_controller.rb
@@ -2,4 +2,22 @@ class PerformanceController < ApplicationController
 
   def index
   end
+
+  def success
+
+  end
+
+  def fail
+
+  end
+
+  def submit
+    # 値の確認だけ必要。金曜日に合流させる予定。
+    if params[:value] == 0
+      redirect_to action: :success
+    else
+      redirect_to action: :fail
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'front_page/index'
-  get 'performance/index'
+  # resources :performance
+  root controller: "front_page", action: :index
+  get '/performance', controller: "performance", action: :index
+  post '/performance/submit', controller: "performance", action: :submit
+  get '/performance/success', controller: "performance", action: :success
+  get '/performance/fail', controller: "performance", action: :fail
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
