04eb2016bdd792c49835f8ceca08a8455f1fd133
daisuke_kumagai <z24099dk@aiit.ac.jp>
2025-01-09 21:02:44 +0900

    create real performance page

--- app/helpers/performance_helper.rb
+++ app/helpers/performance_helper.rb
@@ -2,5 +2,11 @@ module PerformanceHelper
     def random_sound_id
       rand(1..9)
     end
-  end
+
+    def get_ramdom_audio_file
+        ramdom_number = random_sound_id
+        str = ramdom_number.to_s
+        return str + ".mp3"
+    end
+end
   
